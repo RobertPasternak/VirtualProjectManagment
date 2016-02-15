379e4e0a341e798f57b609b532a6d6dc6f1d40c7
Robertosen <kontakt.pasternak@gmail.com>
2016-02-15 23:40:23 +0100

    Dodano AccountController. Dodano podstawowy panel logowania. Dodano style do walidacji. Zmiany w widoku częściowym _Account. Zmiany w domyślnym wyglądzie strony.

---
+++ VirtualProjectManagment/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VirtualProjectManagment.Models;
+
+namespace VirtualProjectManagment.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountModels accountModels)
+        {
+            if (ModelState.IsValid)
+            {
+                if (accountModels.Login == "123" && accountModels.Password == "123")
+                {                  
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Błędne dane logowania");
+            }
+            return View(accountModels);
+        }
+    }
+}
--- VirtualProjectManagment/Models/AccountModels.cs
+++ VirtualProjectManagment/Models/AccountModels.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+using Weborb.Service;
 
 namespace VirtualProjectManagment.Models
 {
     public class AccountModels
     {
+        [Required]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
 
+        [Required]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }     
     }
 }
